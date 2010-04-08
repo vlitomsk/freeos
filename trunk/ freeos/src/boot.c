66a127498a5be7b785f11917a7f033489ba7c16d
shum117@bk.ru <shum117@bk.ru@2687dd45-faa0-e365-b446-d487f533e827>
2010-04-08 09:48:36 +0000

    некая попытка привести в порядок вывод на экран, !!требуется проверка этого на адекватность!!

--- trunk/ freeos/src/boot.c
+++ trunk/ freeos/src/boot.c
@@ -1,20 +1,31 @@
 
 #include <multiboot/multiboot.h>
-#include <screen/simple_io.h>
+#include <screen/screen.h>
 
 void kernel_main()
 {
+    __SCREEN s;
+    setscreen(&s);
 	init_tty();
 	clear(0xF7);
+	gotoxy(31,15);
+	textcolor(0x13);
+	put_str("FreeOS Hello World!");
 	int i = 0;
 	i=10;
 	while(i<90){
 		textcolor(0xF0);
 		gotoxy(i,20);
-		puts("-");
+		put_str("-");
 		delay(100);
 		i++;
-		if (i==71)	{ i=10; clear(0xF7);}
+		if (i==71)	{ 
+		    i=10; 
+		    clear(0xF7); 
+		    gotoxy(31,15);
+	        textcolor(0x13);
+	        put_str("FreeOS Hello World!");
+	    }
 	}
 }
 
---
+++ trunk/ freeos/src/sys/lib/screen/output.c
@@ -0,0 +1,73 @@
+#include <screen/screen.h>
+/*#include <config/video.h>*/
+
+static char* video= VIDEO_RAM;
+
+//Вывод одного символа в режиме телетапа
+void putchar(char c) {  
+  int i;
+  switch (c) {
+
+  	case '\n': //Если это символ новой строки
+  					  defaultscreen->tty_cursor=defaultscreen->tty_cursor+VIDEO_WIDTH-defaultscreen->tty_cursor%VIDEO_WIDTH;  					  
+    				break;
+    
+	  default:
+  					  *(video + defaultscreen->tty_cursor*2) = c;
+					    *(video + 2*(defaultscreen->tty_cursor++)+1) = defaultscreen->tty_attribute;					    
+				    break;
+  }
+
+  //Если курсор вышел за границу экрана, сдвинем экран вверх на одну строку
+  if(defaultscreen->tty_cursor>VIDEO_WIDTH*VIDEO_HEIGHT) {
+    for(i = VIDEO_WIDTH*2; i <= VIDEO_WIDTH*VIDEO_HEIGHT*2+VIDEO_WIDTH*2; ++i)
+      *(video+i-VIDEO_WIDTH*2)=*(video+i);
+    
+    defaultscreen->tty_cursor-=VIDEO_WIDTH;
+  }
+}
+
+//Вывод строки, заканчивающейся нуль-символом
+void put_str(const char *s){
+  while(*s) {
+    putchar(*s);
+    s++;
+  }
+}
+
+void put_int(int a) {
+	if (a == 0) {
+	    put_str("0\0");
+	    return;
+	}
+		
+	//char* mystr = (char*)malloc(sizeof(char) * 100);
+	char mystr[100];
+	int pos = 0;
+ 	if (a < 0) {
+		mystr[pos++] = '-';
+		a = -a;
+	}
+											
+	//int* stack = (int*)malloc(sizeof(int) * 100);
+	int stack[100];
+	int stacklen = 0;
+	for (;a != 0;) {
+		int tmp = 0;
+		tmp = a % 10;
+		a -= tmp;
+		a /= 10;
+																	
+		stack[stacklen++] = tmp;	
+	}
+																																	
+	for (a = stacklen-1; a >= 0; a--) 
+			mystr[pos++] = (char)(stack[a]+48); 
+
+	//	free(stack);
+																																					
+	mystr[pos] = '\0';
+
+	put_str(mystr);
+}
+
