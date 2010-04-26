b1e2f9445fa5f9d9edd210d4ce30880818c62f64
kvas.omsk <kvas.omsk@2687dd45-faa0-e365-b446-d487f533e827>
2010-04-26 06:36:27 +0000

    добавленва

--- trunk/ freeos/src/sys/include/utils.h
+++ trunk/ freeos/src/sys/include/utils.h
@@ -3,4 +3,12 @@
 
 extern void delay(int a);
 
+extern void outb(unsigned short port, unsigned char value);
+extern void outw(unsigned short port, unsigned short value);
+extern void outl(unsigned short port, unsigned long value);
+
+extern unsigned char inb(unsigned short port);
+extern unsigned short inw(unsigned short port);
+extern unsigned long inl(unsigned short port);
+
 #endif
