fd42921bee7da56a94319559b807021925fec00c
kvas.omsk <kvas.omsk@2687dd45-faa0-e365-b446-d487f533e827>
2010-05-02 07:02:25 +0000

    да елки-палки.

--- trunk/ freeos/make.sh
+++ trunk/ freeos/make.sh
@@ -1,9 +1,9 @@
-cd rev
-make
-cd .. 
+cd rev &&
+make &&
+cd .. &&
 mkisofs -b boot/grub/stage2_eltorito -no-emul-boot -boot-load-size 4 -boot-info-table \
    -iso-level 3 -r -J \
    -publisher "FreeOS" \
-   -o ./build/image.iso  ./build/cdrom/ 
+   -o ./build/image.iso  ./build/cdrom/ &&
 qemu -boot d -m 128 -cdrom  './build/image.iso' -net nic,vlan=0 -net user,vlan=0 -localtime 
 
