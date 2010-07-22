c881449fba2e3f9667dc32e707ef72955fbd75b4
kvas.omsk <kvas.omsk@2687dd45-faa0-e365-b446-d487f533e827>
2010-07-22 14:26:16 +0000

    Вот пока написал PCI + ATA(немного недоработано). Не тестировал, GRUB2 обещанного пока еще нет :-(. Вот, еще немного про ATA. Там для чтения-записи требуется массив дескрипторов. С адресом и количеством байт , думаю , все понятно. А если несколько дескрипторов таких, то у непослених флаг ставить в 0, а если последний, то 0x8000. (сейчас допишу поддержку нескольких, просто взялся уже коммитить :)) В данный момент идет работа над FAT32 (потом наверное еще VFAT к нему подцепить нужно будет)

---
+++ header/pci.h
@@ -0,0 +1,26 @@
+#ifndef PCI_H
+#define PCI_H
+
+#define PCI_CONF_ADDR 0xCF8
+#define PCI_CONF_DATA 0xCFC
+
+// максимальное количество
+#define PCI_BUS_COUNT 0x100
+#define PCI_DEV_COUNT 0x20
+#define PCI_FUN_COUNT 0x8
+
+#include <stdlib.h>
+
+struct pci_device {
+    u16 vendor_id;
+	u16 device_id;
+    u32 class_code;
+    u32 base_addr; // bar0..5 , но у которого 0бит взведен
+    u8 bus, dev, func;
+};
+
+extern u32 pci_read_confspace(u8 bus, u8 dev, u8 func, u8 reg);
+// на самом деле ccode - это класс коде :)
+extern int pci_init_device(int startbus, int startdev, int startfunc, u32 ccode, struct pci_device* dev_);
+
+#endif
